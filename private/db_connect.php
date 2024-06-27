ccd43d2d4047e1e687bae813aaf9fcad58d34e8d
ThiagooFrs <100004223+ThiagooFrs@users.noreply.github.com>
2024-06-27 18:53:49 -0300

    Update db_connect.php

--- private/db_connect.php
+++ private/db_connect.php
@@ -2,9 +2,9 @@
 
 // Defina as informações de conexão ao banco de dados
 $db_host = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_name = "unitreino";
+$db_username = "unitre71_thiago";
+$db_password = "5147486Unitreino.";
+$db_name = "unitre71_unitreino";
 
 // Crie a conexão com o banco de dados
 $db = new mysqli($db_host, $db_username, $db_password, $db_name);
